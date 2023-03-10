3760b252dc48da37533cc40f7db5ae2510ca74e8
David Kastner <kastner@mit.edu>
2023-03-09 20:31:02 -0500

    ESP automation

--- qa/process.py
+++ qa/process.py
@@ -1062,6 +1062,62 @@ def get_charge_schemes():
             shutil.copy(new_name, f"{final_dest}{new_name}")   
         os.chdir(owd)
 
+
+def calculate_esp():
+    """
+    Calculate the electrostatic potential (ESP).
+
+    Takes the output from a Multiwfn charge calcualtion and calculates
+    the ESP.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+    df = pd.read_csv("gas_phaseYIDLOP_Voronoi.txt", sep="\s+", names=["Atom", "x", "y", "z", "charge"])
+    # Coulombic constant in kg*m**3/(s**4*A**2)
+    k = 8.987551*(10**9)
+
+    # Convert each column to list for quicker indexing
+    atoms = df["Atom"]
+    charges = df["charge"]
+    xs = df["x"]
+    ys = df["y"]
+    zs = df["z"]
+
+    # Pick the index of the atom at which the esp should be calculated
+    idx_atom = 0
+
+    # Determine position and charge of the target atom
+    xo = xs[idx_atom]
+    yo = ys[idx_atom]
+    zo = zs[idx_atom]
+    chargeo = charges[0]
+    total_esp = 0
+
+    # Unit conversion
+    A_to_m = 10**(-10)
+    KJ_J = 10**-3
+    faraday = 23.06   #kcal/(mol*V)
+    C_e = 1.6023*(10**-19)
+    one_mol = 6.02*(10**23)
+    cal_J = 4.184
+
+    for idx in range(0, len(atoms)):
+        if idx == idx_atom:
+            continue
+        else:
+            # Calculate esp and convert to units (A to m)
+            r = (((xs[idx] - xo)*A_to_m)**2 + ((ys[idx] - yo)*A_to_m)**2 + ((zs[idx] - zo)*A_to_m)**2)**(0.5)
+            total_esp = total_esp + (charges[idx]/r)
+
+    final_esp = k*total_esp*((C_e))*cal_J*faraday   # Note that cal/kcal * kJ/J gives 1
+    print(f"{str(final_esp)} kJ/(mol*e)")
+
+
 if __name__ == "__main__":
     # Run the command-line interface when this script is executed
     average_by_residues("all_charges.xls", "template.pdb")
